f37d197f4c0abbd250c56be02beecc9531ea5dbb
carvestar <165309143+carvestar@users.noreply.github.com>
2024-11-29 10:12:11 +0900

    Update coffee.py

--- coffee.py
+++ coffee.py
@@ -5,7 +5,7 @@ print('=' * 45)
 for category in menu:
     print('{:^15s}'.format(category), end = ' ')
 print()
-print('=' * 4555)
+print('=' * 455)
 
 for ckind in coffee:
     print('{:^10s}'.format(ckind))
